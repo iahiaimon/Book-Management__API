eddc7874bc77f22db9ce99bdb8e4ec44ab0f62be
iahia <iahiaimon555@gmail.com>
2025-05-29 14:04:22 +0600

    Initializing

---
+++ books/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Author (models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15 , unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Book (models.Model):
+    title = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=30 , unique=True)
+    description = models.TextField(null=True , blank= True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
